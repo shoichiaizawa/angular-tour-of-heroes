a600ab9e7ba71883fe98dd2425a735a59afd08ce
Shoichi Aizawa <shoichiaizawa@gmail.com>
2021-03-05 16:17:26 +0900

    feat(heroes): add a details section when click on a hero name

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,13 +9,14 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  selectedHero?: Hero;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
