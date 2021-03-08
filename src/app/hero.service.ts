8e9578aae03df7f672ec57e4a475e31182a9af0f
Shoichi Aizawa <shoichiaizawa@gmail.com>
2021-03-08 12:55:23 +0900

    feat(hero.service): inject message service into hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,11 +4,13 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor() { }
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     return of(HEROES);
